6a667ec4f57bee0e23425a0e64be16200a272a9e
Dan Gautsch <dan.gautsch@thinkbrownstone.com>
2016-07-24 14:34:42 -0400

    Moving navigation to it's own folder

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
 var angular = require('angular');
 
 angular.module('sandboxApp', []);
-
-angular.module('sandboxApp').directive('navigation', function() {
-  return {
-    templateUrl: "navigation.tpl.html",
-    restrict: "E"
-  };
-});
+require('./navigation');
